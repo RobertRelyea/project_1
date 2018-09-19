d82a696dd200f0bcb4f0cbc4da426b7967836bdb
rer3378 <rer3378@g.rit.edu>
2018-09-19 16:36:16 -0400

    Trying more stuff, not really sure whats going on

--- Objects/main.d
+++ Objects/main.d
@@ -9,7 +9,8 @@
 .\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32L4xx_DFP\2.0.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\system_stm32l4xx.h
 .\objects\main.o: Drivers/SysClock.h
 .\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\STM32L4xx_DFP\2.0.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\stm32l476xx.h
-.\objects\main.o: Drivers/LED.h
 .\objects\main.o: Drivers/UART.h
+.\objects\main.o: Drivers/LED.h
+.\objects\main.o: Drivers/input_pa0.h
 .\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
 .\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "stm32l476xx.h"
 #include "Drivers/SysClock.h"
 #include "Drivers/UART.h"
+#include "Drivers/LED.h"
 #include "Drivers/input_pa0.h"
 
 #include <string.h>
@@ -8,7 +9,8 @@
 
 char RxComByte = 0;
 uint8_t buffer[BufferSize];
-char str[] = "Give Red LED control input (Y = On, N = off):\r\n";
+char goodCount[] = "Count > 0";
+char badCount[] = "Count <= 0";
 char newline[]="\r\n";
 int buffer_index=0;
 int main(void){
@@ -16,40 +18,50 @@ int main(void){
 	
 	System_Clock_Init(); // Switch System Clock = 80 MHz
 	
+	LED_Init();
+	
 	init_pa0(); // Init PA0 to alternate function mode
+	monitor_pa0();
 	
 	RCC->APB1ENR1 |= 0x0001; // Enable TIM2 in the APB1 clock enable register 1
 	
 	TIM2->PSC = 0x9C40; // Set prescaler for TIM2 to 40000
 										 // Results in a sampling frequency of 2 KHz
 
-	TIM2->EGR |=0X40; // Trigger an update event for timer 2
+	TIM2->EGR |=0x1; // Trigger an update event for timer 2
 	
 	TIM2->CCER &= ~0x1; // Turn off output enable for capture input
 											// Ensures changes will take effect when capture input is reenabled
 	
-	// Set up CCMRx please
+	// Set up CCMRx
+	TIM2->CCMR1 |= 0x1; // Set CC1 channel as input, IC1 mapped on TI2
+	TIM2->CCMR1 &= ~0xF0; // Clear input capture 1 filter
+	
+	TIM2->CCER &= ~0xD; // Set our capture trigger to rising edges
+	
+	TIM2->CCER |= 0x1; // Turn on output enable for capture input
+	
+	
+	
+	TIM2->CR1 |= 0x1; // Enable input capture
 	
 	UART2_Init();
 		
-	while (1){
-		
-		rxByte = USART_Read(USART2); //Read the input and store it into rxByte
-		USART_Write(USART2, (uint8_t *)&rxByte, 1); //Pointing to rxBytes address
-		if(rxByte=='\r')//If input is enter
+	while (1)
+	{	
+		if (TIM2->SR & 0X2) // Capture 1 interrupt flag
 		{
+			// Read current TIM2 channel 1 counter value
+			unsigned int count = TIM2->CCR1;
+			
+			sprintf((char *)buffer, "CCR1 Value = %d", count);
+			
+			USART_Write(USART2,(uint8_t *)buffer, strlen((char *)buffer)); //move to a new line
 			USART_Write(USART2,(uint8_t *)newline, strlen(newline)); //move to a new line
 			
 			USART_Write(USART2, buffer, buffer_index);//write from buffer
-			buffer_index=0;        //reset buffer to 0
-			USART_Write(USART2,(uint8_t *)newline, strlen(newline)); //move to a new line again
-		}
-		else
-		{
-			buffer[buffer_index]=rxByte;   //put the input in buffer
-			buffer_index++;                //increment buffer as input is being added
-		}		
-		
+			USART_Write(USART2,(uint8_t *)newline, strlen(newline)); //move to a new line
+		}			
 	}
 }
 
